385e137529ef50f0677cb2ae5f94d187e48e3cea
dangoar5 <dgomeza5@gmail.com>
2026-07-08 16:49:35 -0500

    Delete Dashboard directory

--- Dashboard/js/data.js
+++
@@ -1,8 +0,0 @@
-const mercado = JSON.parse(localStorage.getItem("mercado")) || [];
-
-const alertas = JSON.parse(localStorage.getItem("alertas")) || [
-    {
-        titulo: "Sin alertas",
-        descripcion: "Todo está al día."
-    }
-];
